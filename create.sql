b273903e48d6c50f5e9a3ba896b807830f2cf146
Vadim Tkachenko <vadim@percona.com>
2017-03-29 13:19:37 -0700

    Create create.sql

---
+++ create.sql
@@ -0,0 +1,18 @@
+CREATE TABLE lineorder ( LO_ORDERKEY    UInt32,
+        LO_LINENUMBER   UInt8,
+        LO_CUSTKEY              UInt32,
+        LO_PARTKEY              UInt32,
+        LO_SUPPKEY              UInt32,
+        LO_ORDERDATE    Date,
+        LO_ORDERPRIORITY String,
+        LO_SHIPPRIORITY UInt8,
+        LO_QUANTITY             UInt8,
+        LO_EXTENDEDPRICE UInt32,
+        LO_ORDTOTALPRICE UInt32,
+        LO_DISCOUNT             UInt8,
+        LO_REVENUE              UInt32,
+        LO_SUPPLYCOST   UInt32,
+        LO_TAX                  UInt8,
+        LO_COMMITDATE   Date,
+        LO_SHIPMODE             String
+)Engine=MergeTree(LO_ORDERDATE,(LO_ORDERKEY,LO_LINENUMBER,LO_ORDERDATE),8192);
