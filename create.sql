c30da3cfedf432408bdb5f09727b2bb4bcaff30c
Vadim Tkachenko <vadim@percona.com>
2017-04-11 14:22:01 -0700

    Update create.sql

--- create.sql
+++ create.sql
@@ -30,5 +30,20 @@ CREATE TABLE customer (
         C_FAKEDATE      Date
 )Engine=MergeTree(C_FAKEDATE,(C_CUSTKEY,C_FAKEDATE),8192)
 
-CREATE TABLE lineorderd AS lineorder ENGINE = Distributed(perftest_3shards_1replicas, default, lineorder, LO_CUSTKEY);
-CREATE TABLE customerd AS customer ENGINE = Distributed(perftest_3shards_1replicas, default, customer, C_CUSTKEY);
+CREATE TABLE part ( 
+        P_PARTKEY       UInt32,
+        P_NAME          String,
+        P_MFGR          String,
+        P_CATEGORY      String,
+        P_BRAND         String,
+        P_COLOR         String,
+        P_TYPE          String,
+        P_SIZE          Uint8,
+        P_CONTAINER     String,
+        P_FAKEDATE      Date
+)Engine=MergeTree(P_FAKEDATE,(P_PARTKEY,P_FAKEDATE),8192)
+
+CREATE TABLE lineorderd AS lineorder ENGINE = Distributed(perftest_3shards_1replicas, default, lineorder, rand());
+CREATE TABLE customerd AS customer ENGINE = Distributed(perftest_3shards_1replicas, default, customer, rand());
+CREATE TABLE partd AS part ENGINE = Distributed(perftest_3shards_1replicas, default, part, rand());
+
