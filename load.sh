4f1fa04897e6c091ac2bb07b195f76cab34338a5
Vadim Tkachenko <vadim@percona.com>
2017-05-19 09:22:20 -0700

    Update load.sh

--- load.sh
+++ load.sh
@@ -1,3 +1,4 @@
 cat cust.sql | clickhouse-client --host=ch
+sed -i 's/$/"1999-01-01"/' customer.tbl
 cat customer.tbl | clickhouse-client --host=ch --query "INSERT INTO customer FORMAT CSV"
 cat lineorder.tbl | clickhouse-client --host=ch --query "INSERT INTO lineorder FORMAT CSV" 
